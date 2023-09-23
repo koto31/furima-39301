a579307f9d63b05f71deb6b2f3a3b0c53fd2264e
koto31 <136156845+koto31@users.noreply.github.com>
2023-09-23 20:32:00 +0900

    ユーザー管理機能修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
   #has_many :orders
 
   validates :nickname, presence: true
-  validates :password, presence: true, format:{with: /\A[a-zA-Z0-9]+\z/} 
-  validates :last_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/}
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format:{with: VALID_PASSWORD_REGEX} 
+  validates :last_name, presence: true, format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name, presence: true, format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :last_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
   validates :first_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
   validates :birth_date, presence: true
