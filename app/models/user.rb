597502a0e22c7d59977655f6af5238a34c899f51
koto31 <136156845+koto31@users.noreply.github.com>
2023-09-23 20:15:19 +0900

    ユーザー管理機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
-  has_many :orders
+  #has_many :items
+  #has_many :orders
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :password, presence: true, format:{with: /\A[a-zA-Z0-9]+\z/} 
+  validates :last_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
   validates :birth_date, presence: true
 end
